ff20972ab16dbe6c1d1544a89d21c3a689833c09
Ekaterina Kazakova <30477033+vrestles@users.noreply.github.com>
2019-03-30 19:46:23 +0300

    Update check_lex_errors.c

--- assembler/src/lex_errors/check_lex_errors.c
+++ assembler/src/lex_errors/check_lex_errors.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/asm.h"
+#include "asm.h"
 
 static int	check_lex_errors(t_lexem *lexem)
 {
