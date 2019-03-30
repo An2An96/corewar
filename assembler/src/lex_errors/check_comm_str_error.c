596509681c1b0bd8382cf90e9283bddbbc285800
Ekaterina Kazakova <30477033+vrestles@users.noreply.github.com>
2019-03-30 19:46:06 +0300

    Update check_comm_str_error.c

--- assembler/src/lex_errors/check_comm_str_error.c
+++ assembler/src/lex_errors/check_comm_str_error.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/asm.h"
+#include "asm.h"
 
 int		check_error_command(char *value)
 {
